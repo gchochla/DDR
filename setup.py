d945753428ef98ba9617b9f2514cb0721d930c9d
Georgios Chochlakis <georgioschochlakis@gmail.com>
2021-11-06 20:22:06 -0700

    Update setup requirements

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='ddr',
-      version='0.1',
-      description='Functions for implementing and exploring Distributed Dictionary Representation',
-      author='Joe Hoover',
-      author_email='jehoover@usc.edu',
-      license='USC',
-      packages=['ddr'],
-      package_data = {'ddr': ['*.pdf', '*.txt']},
-      zip_safe=False)
-
+setup(
+    name="ddr",
+    version="0.2",
+    description="Functions for implementing and exploring Distributed Dictionary Representation",
+    author="Joe Hoover, Georgios Chochlakis",
+    author_email="jehoover@usc.edu, chochlak@usc.edu",
+    license="USC",
+    packages=find_packages(),
+    package_data={"ddr": ["*.pdf", "*.txt"]},
+    zip_safe=False,
+    install_requires=[
+        "numpy==1.21.4",
+        "pandas==1.3.4",
+        "cython==0.29.24",
+        "gensim==4.1.2",
+    ],
+)
